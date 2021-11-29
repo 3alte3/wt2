71ffd6120457d86cb0fa03c0516c42f80e0bd1ae
3alte3 <87598968+3alte3@users.noreply.github.com>
2021-11-29 10:25:50 +0300

    Add files via upload

---
+++ src/main/java/by/tc/task01/dao/DAOFactory.java
@@ -0,0 +1,43 @@
+package main.java.by.tc.task01.dao;
+
+import main.java.by.tc.task01.dao.impl.ApplianceDAOImpl;
+
+/**
+ * Factory class for data access objects to appliance entities
+ *
+ *
+ */
+public final class DAOFactory {
+	/**
+	 * single factory instance for all the application
+	 */
+	private static final DAOFactory instance = new DAOFactory();
+	/**
+	 * DAO to appliance entities
+	 */
+	private final ApplianceDAO applianceDAO = new ApplianceDAOImpl();
+
+	/**
+	 * constructor unavailable from out of class
+	 */
+	private DAOFactory() {
+	}
+
+	/**
+	 * Factory method for DAO
+	 * 
+	 * @return ApplianceDAO - DAO to appliance entities
+	 */
+	public ApplianceDAO getApplianceDAO() {
+		return applianceDAO;
+	}
+
+	/**
+	 * Static factory instance returning method instead of constructor
+	 * 
+	 * @return single factory instance for all the application
+	 */
+	public static DAOFactory getInstance() {
+		return instance;
+	}
+}
